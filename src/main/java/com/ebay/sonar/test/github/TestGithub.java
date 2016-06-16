5fcee39e7361ce9f20a9bf2aee80c69d5bc17bcb
dsongebay <dsong@ebay.com>
2016-06-16 10:03:48 +0800

    Update TestGithub.java

--- src/main/java/com/ebay/sonar/test/github/TestGithub.java
+++ src/main/java/com/ebay/sonar/test/github/TestGithub.java
@@ -4,6 +4,15 @@ public class TestGithub {
 
 	public void testJava(){
 		System.out.println("test");
+		 if ( 3 != 3 ) { // always false
+  	System.out.println("test");
+}
+if ( 3 == 3 && 3 == 3 ) { // if the first one is true, the second one is too
+ 	System.out.println("test");
+}
+if ( 3== 3 || 3 == 3) { // if the first one is true, the second one is too
+  	System.out.println("test");
+}
 	}
 	
 }
